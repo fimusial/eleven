53ad4b0daaec2ad64f8faca332335232ebc9d37c
Filip Musial <fimusial@gmail.com>
2022-08-04 23:58:34 +0200

    contact form validation

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,15 +9,15 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
+  public readonly pdf = 'application/pdf';
   public done: boolean = false;
   public attachment: File | null = null;
 
   public form = new FormGroup({
-    senderName: new FormControl<string>(''),
-    replyTo: new FormControl<string>(''),
-    subject: new FormControl<string>(''),
-    message: new FormControl<string>(''),
+    senderName: new FormControl<string>('', { validators: [Validators.required], updateOn: 'blur' }),
+    replyTo: new FormControl<string>('', { validators: [Validators.required, Validators.email], updateOn: 'blur' }),
+    subject: new FormControl<string>('', { validators: [Validators.required], updateOn: 'blur' }),
+    message: new FormControl<string>('', { validators: [Validators.required], updateOn: 'blur' }),
   });
 
   constructor(private http: HttpClient) {
@@ -30,27 +30,25 @@ export class ContactFormComponent implements OnInit {
   }
 
   public onFormSubmitted(): void {
-    // validate first
+    if (!this.form.valid || (this.attachment && this.attachment?.type !== this.pdf)) {
+      return;
+    }
+    
     const formData = new FormData();
     formData.append('sender-name', this.form.value.senderName as string);
     formData.append('reply-to', this.form.value.replyTo as string);
     formData.append('subject', this.form.value.subject as string);
     formData.append('message', this.form.value.message as string);
     formData.append('attachment', this.attachment as File, this.attachment?.name);
-    
-    this.http.post(environment.mailerUrl, formData)
-      .subscribe(() => {
-        this.done = true;
-      });
+
+    this.http.post(environment.mailerUrl, formData).subscribe(() => {});
+    this.done = true;
   }
 
   public onAttachmentChanged(event: Event): void {
     const htmlInput = event.target as HTMLInputElement;
-    
-    if (!(htmlInput && htmlInput.files && htmlInput.files.length > 0)) {
-      return;
-    }
-
-    this.attachment = htmlInput.files[0];
+    this.attachment = htmlInput && htmlInput.files && htmlInput.files.length > 0
+    ? htmlInput.files[0]
+    : null;
   }
 }
